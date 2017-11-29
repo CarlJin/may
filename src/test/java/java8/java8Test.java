75760413fe3c8626c3cc358659e0e138d379a89b
jintao <jintao@nonobank.com>
2017-11-29 14:30:22 +0800

    update

---
+++ src/test/java/java8/java8Test.java
@@ -0,0 +1,22 @@
+package java8;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+
+/**
+ * @ClassName:
+ * @desc:
+ * @author: jintao
+ * @date: 2017-11-29 下午2:23
+ **/
+public class java8Test {
+
+    private static Logger logger = LoggerFactory.getLogger(java8Test.class);
+
+    @Test
+    private void timeTest() {
+        logger.info("hello");
+    }
+}
